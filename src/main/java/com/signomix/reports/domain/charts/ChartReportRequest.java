aea59753dc932addcb2cb8d8b84c45b78a46564a
gskorupa <g.skorupa@gmail.com>
2026-05-26 23:32:18 +0200

    Add ChartReportRequest class
    Introduce ChartReportRequest DTO holding ChartDefinition and
    ReportDefinition

---
+++ src/main/java/com/signomix/reports/domain/charts/ChartReportRequest.java
@@ -0,0 +1,9 @@
+package com.signomix.reports.domain.charts;
+
+import com.signomix.common.db.ReportDefinition;
+
+public class ChartReportRequest {
+
+    public ChartDefinition chartDefinition;
+    public ReportDefinition reportDefinition;
+}
